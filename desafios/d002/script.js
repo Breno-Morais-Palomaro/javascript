3093a113898465d26754cca52d28cb440edb4ccb
BrenoMop <brmoraispalo@gmail.com>
2022-10-25 11:39:19 -0300

    mano
    faz tanto tempo que nem lembro lol

---
+++ desafios/d002/script.js
@@ -0,0 +1,5 @@
+function start() {
+    var nome = prompt ('Qual seu nome?')
+    var idade = prompt (`Olá, ${nome}! Quantos anos você tem?`)
+    alert (`Acabei de conhecer ${nome}, que tem ${idade} anos de idade!`)
+}
---
+++ desafios/d003/script.js
@@ -0,0 +1,6 @@
+function start() {
+    var num = Number(prompt ('Digite um númeiro inteiro qualquer.'))
+    var soma = num + 1
+    var subt = num - 1
+    Number(alert (`Antes do ${num}, temos o número ${subt} ____ Depois do ${num}, temos o número ${soma}.`))
+}
